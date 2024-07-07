53541a5b2bd18a8b1ec9f769b7d628daf82e95a0
Chirag <118044484+chiragHimself@users.noreply.github.com>
2024-07-07 13:50:04 +0530

    Add files via upload

---
+++ writerOP.py
@@ -0,0 +1,115 @@
+from crewai import Agent, Task, Crew
+from langchain.llms import Ollama
+from dotenv import load_dotenv
+import streamlit as st 
+
+#setting up the llms
+load_dotenv()
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+os.environ["OPENAI_API_KEY"] = "NA"
+
+llm=ChatGoogleGenerativeAI(model="gemini-1.5-flash",
+                           verbose=True,
+                           temperature=0.5,
+                           google_api_key=os.getenv("GOOGLE_API_KEY"))
+
+
+#creating agents for the task.
+planner = Agent(
+    role="Content Planner",
+    goal="Plan engaging and factually accurate content on {topic}",
+    backstory="You're working on planning a blog article "
+              "about the topic: {topic}."
+              "You collect information that helps the "
+              "audience learn something "
+              "and make informed decisions. "
+              "Your work is the basis for "
+              "the Content Writer to write an article on this topic.",
+    allow_delegation=False,
+	verbose=True,
+    llm = llm
+)
+
+writer = Agent(
+    role="Content Writer",
+    goal="Write insightful and factually accurate "
+         "opinion piece about the topic: {topic}",
+    backstory="You're working on a writing "
+              "a new opinion piece about the topic: {topic}. "
+              "You generate outputs of around 300 words on maximum"
+              "You base your writing on the work of "
+              "the Content Planner, who provides an outline "
+              "and relevant context about the topic. "
+              "You follow the main objectives and "
+              "direction of the outline, "
+              "as provide by the Content Planner. "
+              "You also provide objective and impartial insights "
+              "and back them up with information "
+              "provide by the Content Planner. "
+              "You acknowledge in your opinion piece "
+              "when your statements are opinions "
+              "as opposed to objective statements.",
+    allow_delegation=False,
+    verbose=True,
+    llm = llm
+)
+
+
+
+#defining the tasks to be performed.
+
+
+plan = Task(
+    description=(
+        "1. Prioritize the latest trends, key players, "
+            "and noteworthy news on {topic}.\n"
+        "2. Identify the target audience, considering "
+            "their interests and pain points.\n"
+        "3. Develop a detailed content outline including "
+            "an introduction, key points, and a call to action.\n"
+        "4. Include SEO keywords and relevant data or sources."
+    ),
+    expected_output="A comprehensive content plan document "
+        "with an outline, audience analysis, "
+        "SEO keywords, and resources.",
+    agent=planner,
+)
+
+write = Task(
+    description=(
+        "1. Use the content plan to craft a compelling "
+            "blog post on {topic}.\n"
+        "2. Incorporate SEO keywords naturally.\n"
+		"3. Sections/Subtitles are properly named "
+            "in an engaging manner.\n"
+        "4. Ensure the post is structured with an "
+            "engaging introduction, insightful body, "
+            "and a summarizing conclusion.\n"
+        "5. Proofread for grammatical errors and "
+            "alignment with the brand's voice.\n"
+    ),
+    expected_output="A well-written blog post "
+        "in markdown format, ready for publication, "
+        "each section should have 2 or 3 paragraphs.",
+    agent=writer,
+)
+
+
+
+crew = Crew(
+    agents=[planner, writer],
+    tasks=[plan, write],
+    verbose=2
+)
+st.title ("Blog Writer")
+query = st.text_input("Enter the topic:")
+
+if st.button("Submit"):
+    if query:
+        response = crew.kickoff(inputs={"topic": query})
+        st.write(response)
+#result = crew.kickoff(inputs={"topic": "Artificial Intelligence"})
+
+
+
