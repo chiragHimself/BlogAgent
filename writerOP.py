0278aa9181d01a9cc1f5e0dbc037a5ad4760f1d8
Chirag <118044484+chiragHimself@users.noreply.github.com>
2024-07-08 10:36:46 +0530

    Update writerOP.py

--- writerOP.py
+++ writerOP.py
@@ -1,115 +1,135 @@
 from crewai import Agent, Task, Crew
-from langchain.llms import Ollama
 from dotenv import load_dotenv
-import streamlit as st 
-
-#setting up the llms
-load_dotenv()
-from langchain_google_genai import ChatGoogleGenerativeAI
+import streamlit as st
 import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# Load environment variables
+load_dotenv()
+
+# Set API key environment variable
 os.environ["OPENAI_API_KEY"] = "NA"
+#tracing with langsmith
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
-llm=ChatGoogleGenerativeAI(model="gemini-1.5-flash",
-                           verbose=True,
-                           temperature=0.5,
-                           google_api_key=os.getenv("GOOGLE_API_KEY"))
+# Initialize LLM
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    verbose=True,
+    temperature=0.5,
+    google_api_key=os.getenv("GOOGLE_API_KEY")
+)
 
-
-#creating agents for the task.
+# Define agents
 planner = Agent(
     role="Content Planner",
     goal="Plan engaging and factually accurate content on {topic}",
-    backstory="You're working on planning a blog article "
-              "about the topic: {topic}."
-              "You collect information that helps the "
-              "audience learn something "
-              "and make informed decisions. "
-              "Your work is the basis for "
-              "the Content Writer to write an article on this topic.",
+    backstory="You're working on planning a blog article about the topic: {topic}. "
+              "You collect information that helps the audience learn something "
+              "and make informed decisions. Your work is the basis for the Content Writer to write an article on this topic.",
     allow_delegation=False,
-	verbose=True,
-    llm = llm
+    verbose=True,
+    llm=llm
 )
 
 writer = Agent(
     role="Content Writer",
-    goal="Write insightful and factually accurate "
-         "opinion piece about the topic: {topic}",
-    backstory="You're working on a writing "
-              "a new opinion piece about the topic: {topic}. "
-              "You generate outputs of around 300 words on maximum"
-              "You base your writing on the work of "
-              "the Content Planner, who provides an outline "
-              "and relevant context about the topic. "
-              "You follow the main objectives and "
-              "direction of the outline, "
-              "as provide by the Content Planner. "
-              "You also provide objective and impartial insights "
-              "and back them up with information "
-              "provide by the Content Planner. "
-              "You acknowledge in your opinion piece "
-              "when your statements are opinions "
-              "as opposed to objective statements.",
+    goal="Write an insightful and factually accurate opinion piece about the topic: {topic}",
+    backstory="You're working on writing a new opinion piece about the topic: {topic}. "
+              "You base your writing on the work of the Content Planner, who provides an outline and relevant context about the topic. "
+              "You follow the main objectives and direction of the outline, providing objective and impartial insights backed by information provided by the Content Planner.",
     allow_delegation=False,
     verbose=True,
-    llm = llm
+    llm=llm
 )
 
+editor = Agent(
+    role="Editor",
+    goal="Edit a given blog post to align with the writing style of the organization and ensure the final output shall be of maximum {limit} words.",
+    backstory="You are an editor who receives a blog post from the Content Writer. "
+              "Your goal is to review the blog post to ensure that it follows journalistic best practices, provides balanced viewpoints, "
+              "and avoids major controversial topics or opinions when possible.",
+    allow_delegation=False,
+    verbose=True,
+    llm=llm
+)
 
-
-#defining the tasks to be performed.
-
-
+# Define tasks
 plan = Task(
     description=(
-        "1. Prioritize the latest trends, key players, "
-            "and noteworthy news on {topic}.\n"
-        "2. Identify the target audience, considering "
-            "their interests and pain points.\n"
-        "3. Develop a detailed content outline including "
-            "an introduction, key points, and a call to action.\n"
+        "1. Prioritize the latest trends, key players, and noteworthy news on {topic}.\n"
+        "2. Identify the target audience, considering their interests and pain points.\n"
+        "3. Develop a detailed content outline including an introduction, key points, and a call to action.\n"
         "4. Include SEO keywords and relevant data or sources."
     ),
-    expected_output="A comprehensive content plan document "
-        "with an outline, audience analysis, "
-        "SEO keywords, and resources.",
+    expected_output="A comprehensive content plan document with an outline, audience analysis, SEO keywords, and resources.",
     agent=planner,
 )
 
 write = Task(
     description=(
-        "1. Use the content plan to craft a compelling "
-            "blog post on {topic}.\n"
+        "1. Use the content plan to craft a compelling blog post on {topic}.\n"
         "2. Incorporate SEO keywords naturally.\n"
-		"3. Sections/Subtitles are properly named "
-            "in an engaging manner.\n"
-        "4. Ensure the post is structured with an "
-            "engaging introduction, insightful body, "
-            "and a summarizing conclusion.\n"
-        "5. Proofread for grammatical errors and "
-            "alignment with the brand's voice.\n"
+        "3. Sections/Subtitles are properly named in an engaging manner.\n"
+        "4. Ensure the post is structured with an engaging introduction, insightful body, and a summarizing conclusion.\n"
+        "5. Proofread for grammatical errors and alignment with the brand's voice."
     ),
-    expected_output="A well-written blog post "
-        "in markdown format, ready for publication, "
-        "each section should have 2 or 3 paragraphs.",
+    expected_output="A well-written blog post in markdown format, ready for publication, each section should have 2 or 3 paragraphs.",
     agent=writer,
 )
 
-
+edit = Task(
+    description=("Proofread the given blog post for grammatical errors and alignment with the brand's voice. Must trim down the response to {limit} words."),
+    expected_output="A well-written blog post in markdown format, ready for publication, each section should have 2 or 3 paragraphs.",
+    agent=editor
+)
 
 crew = Crew(
-    agents=[planner, writer],
-    tasks=[plan, write],
+    agents=[planner, writer, editor],
+    tasks=[plan, write, edit],
     verbose=2
 )
-st.title ("Blog Writer")
-query = st.text_input("Enter the topic:")
 
+# Custom CSS for better styling
+st.markdown("""
+    <style>
+    .main-title {
+        color: #FFF8F3;
+        font-family: 'Baskervville SC',Roboto;
+        text-align: center;
+    }
+    .subheader {
+        color: #758694;
+        font-family: 'Roboto', Baskervville SC;
+        text-align: center;
+    }
+    .input-section {
+        margin: 20px 0;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+# Streamlit App
+st.markdown('<h1 class="main-title">Article Dada 👑</h1>', unsafe_allow_html=True)
+st.markdown('<h2 class="subheader">Generate comprehensive articles in less than 15 seconds</h2>', unsafe_allow_html=True)
+
+# Input section
+st.markdown('<div class="input-section">', unsafe_allow_html=True)
+query = st.text_input("Enter the topic:", placeholder="e.g., Artificial Intelligence")
+limit = st.text_input("Enter the word limit in words:", placeholder="e.g., 500")
+st.markdown('</div>', unsafe_allow_html=True)
+
+st.markdown("###### Please note the word limit might vary by 50 words to ensure a perfect response in few cases.")
+
+# Submit button
 if st.button("Submit"):
     if query:
-        response = crew.kickoff(inputs={"topic": query})
-        st.write(response)
-#result = crew.kickoff(inputs={"topic": "Artificial Intelligence"})
-
-
-
+        try:
+            with st.spinner('Article generating...  have some water meanwhile🥤'):
+                response = crew.kickoff(inputs={"topic": query, "limit": limit})
+                st.success("Article generated successfully!")
+                st.write(response)
+        except Exception as e:
+            st.error("Error: Please enter a valid title and avoid controversial topics as we won't respond to that. Thanks!")
+    else:
+        st.warning("Please enter a topic to generate!")
