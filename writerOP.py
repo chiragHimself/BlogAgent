ebae935b70470b76afb7ef100716025fa50b5100
Chirag <118044484+chiragHimself@users.noreply.github.com>
2024-07-08 11:48:30 +0530

    Update writerOP.py

--- writerOP.py
+++ writerOP.py
@@ -1,3 +1,10 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+import sqlite3
+
+
 from crewai import Agent, Task, Crew
 from dotenv import load_dotenv
 import streamlit as st
